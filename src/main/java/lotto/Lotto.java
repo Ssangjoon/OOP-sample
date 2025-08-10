e19aad0a88da917c9dcae243dcfbd8f70d01161d
ssang <qkqh34k@gmail.com>
2025-08-10 17:19:11 +0900

    Lotto test

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,6 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.*;
 
 public class Lotto {
     private final Set<LottoNum> lottoNumbers;
@@ -23,13 +17,30 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         Set<LottoNum>  lottoNumbers = new HashSet<>();
+
+        validateLength(numbers);
+
         for(int number : numbers){
             LottoNum lottoNum = LottoNum.of(number);
             lottoNumbers.add(lottoNum);
         }
+
+        if(lottoNumbers.size() < LENGTH){
+            throw new IllegalArgumentException("서로 다른 6개의 숫자를 입력하세요.");
+        }
+
         this.lottoNumbers = lottoNumbers;
     }
 
+    private void validateLength(Collection<Integer> numbers) {
+        if (numbers.size() != LENGTH) {
+            throw new IllegalArgumentException("로또 번호는 서로 다른 6자리 숫자로 이루어져야 합니다.");
+        }
+    }
+
+    public static Lotto of(List<Integer> numbers){
+        return new Lotto(numbers);
+    }
 
     public Set<LottoNum> getLottoNumbers() {
         return lottoNumbers;
---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,44 @@
+package lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTest {
+    private Lotto lotto;
+
+    @BeforeEach
+    public void setup() {
+        lotto = Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    @DisplayName("Lotto는 LottoNum 6개로 구성된다.")
+    public void createLottoNumbersTest() {
+        assertThat(lotto.getLottoNumbers().size()).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Lotto 가 서로 다른 6자리 숫자로 이루어지지 않으면 IllegalArgumentException 가 발생한다")
+    @ValueSource(strings = {"1,2,3,4,5", "1,2,3,4,5,6,7", "1,1,2,3,4,5", "1,1,2,3,4,5,7"})
+    public void validationLottoNumbersTest(String notValidInput) {
+        String[] split = notValidInput.split(",");
+        List<Integer> numbers = Stream.of(split)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        assertThatThrownBy(() -> Lotto.of(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
